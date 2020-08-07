173563ab886919f8ed82263f287f3880b7bd5a27
John A. De Goes <john@degoes.net>
2020-08-06 20:43:10 -0400

    first commit

---
+++ docs/about/index.md
@@ -0,0 +1,8 @@
+---
+id: about_index
+title:  "About ZIO Cache"
+---
+
+TODO: Tagline
+
+TODO: Long description (paragraph)
---
+++ zio-cache/shared/src/main/scala/zio/cache/HelloWorld.scala
@@ -0,0 +1,17 @@
+package zio.cache
+
+import zio.App
+import zio.console._
+
+object HelloWorld extends App {
+
+  def run(args: List[String]) =
+    myAppLogic.exitCode
+
+  val myAppLogic =
+    for {
+      _    <- putStrLn("Hello! What is your name?")
+      name <- getStrLn
+      _    <- putStrLn(s"Hello, $name, welcome to ZIO!")
+    } yield ()
+}
