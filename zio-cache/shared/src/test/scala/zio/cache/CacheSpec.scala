0ef17f14f84c2403a84fdd2cc6e78cbf6fd52f69
Adam Fraser <adam.fraser@gmail.com>
2021-06-15 22:58:53 -0700

    Upgrade Scala Version (#29)
    * upgrade scala version
    
    * enable compiler plugin
    
    * fix scala native issue

--- zio-cache/shared/src/test/scala/zio/cache/CacheSpec.scala
+++ zio-cache/shared/src/test/scala/zio/cache/CacheSpec.scala
@@ -1,26 +1,16 @@
 package zio.cache
 
 import zio._
-import zio.blocking.Blocking
-import zio.clock.Clock
 import zio.duration._
-import zio.random.Random
 import zio.test.Assertion._
 import zio.test._
-import zio.test.environment.{Live, TestClock, TestConsole, TestRandom, TestSystem}
 
 object CacheSpec extends DefaultRunnableSpec {
 
   def hash(x: Int): Int => UIO[Int] =
     y => ZIO.succeed((x ^ y).hashCode)
 
-  def spec: Spec[Has[Annotations.Service] with Has[Live.Service] with Has[Sized.Service] with Has[
-    TestClock.Service
-  ] with Has[TestConfig.Service] with Has[TestConsole.Service] with Has[TestRandom.Service] with Has[
-    TestSystem.Service
-  ] with Has[Clock.Service] with Has[zio.console.Console.Service] with Has[zio.system.System.Service] with Has[
-    Random.Service
-  ] with Has[Blocking.Service], TestFailure[Any], TestSuccess] = suite("CacheSpec")(
+  def spec: ZSpec[Environment, Failure] = suite("CacheSpec")(
     testM("cacheStats") {
       checkM(Gen.anyInt) { salt =>
         for {
