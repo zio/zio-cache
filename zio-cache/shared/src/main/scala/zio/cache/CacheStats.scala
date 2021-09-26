d1079b3b0ccb684a7612c4853fffec6ca67a6cd4
Terry Lin <tlin.gta@gmail.com>
2021-09-26 11:02:10 -0400

    Added size and capacity to `CacheStats`.

--- zio-cache/shared/src/main/scala/zio/cache/CacheStats.scala
+++ zio-cache/shared/src/main/scala/zio/cache/CacheStats.scala
@@ -4,4 +4,4 @@ package zio.cache
  * `CacheStats` represents a snapshot of statistics for the cache as of a
  * point in time.
  */
-final case class CacheStats(hits: Long, misses: Long)
+final case class CacheStats(hits: Long, misses: Long, size: Int, capacity: Int)
